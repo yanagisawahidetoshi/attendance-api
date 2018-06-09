7bd2719765b826229cfbab99212e19ba4d2e16c8
YanagisawaHidetoshi <yanagisawa@ultrasevenstar.com>
2018-06-09 22:26:15 +0900

    deviseログインまで完成

---
+++ app/serializers/v1/session_serializer.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module V1
+  class SessionSerializer < ActiveModel::Serializer
+    attributes :email, :token_type, :user_id, :access_token
+
+    def user_id
+      object.id
+    end
+
+    def token_type
+      'Bearer'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, only: []
+
+  namespace :v1, defaults: { format: :json } do
+    resource :login, only: [:create], controller: :sessions
+    resource :users, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
